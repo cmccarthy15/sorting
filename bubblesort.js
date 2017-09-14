82a75225668ec3ce256afebe987c4b1231b3b7a5
Caryn McCarthy <cmccarthy15@gmail.com>
2017-09-14 15:29:29 -0400

    updated bubblesort

--- bubblesort.js
+++ bubblesort.js
@@ -1,10 +1,9 @@
-var spyTester = {};
-spyTester.bubbleSort = function(array) {
+function bubbleSort(array) {
   for (var lastItem = array.length - 1; lastItem > 0; lastItem--){
     for (var i = 0; i < lastItem; i++){
       var first = array[i];
       var second = array[i + 1];
-      if (this.swap(first, second)){
+      if (swap(first, second)){
         //we need to call swap
         array[i] = second;
         array[i + 1] = first;
@@ -17,7 +16,7 @@ spyTester.bubbleSort = function(array) {
 // swap is actually used to see how many comparisons are made
 // not how many swaps are made
 // how could we use swap to count that instead?
-spyTester.swap = function(num1, num2) {
+function swap(num1, num2) {
   return num1 > num2;
 
 }
--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -1,30 +1,30 @@
 describe('Bubble Sort', function(){
 
     beforeEach(function() {
-      spyOn(spyTester, 'swap').and.callThrough();
+      spyOn(window, 'swap').and.callThrough();
     });
     it('handles an empty array', function(){
-      expect( spyTester.bubbleSort([]) ).toEqual( [] );
-      expect(spyTester.swap.calls.count()).toEqual(0);
+      expect(bubbleSort([]) ).toEqual( [] );
+      expect(swap.calls.count()).toEqual(0);
     });
     it('handles one item', function(){
-      expect( spyTester.bubbleSort([8]) ).toEqual( [8] );
-      expect(spyTester.swap.calls.count()).toEqual(0);
+      expect(bubbleSort([8]) ).toEqual( [8] );
+      expect(swap.calls.count()).toEqual(0);
     });
     it('handles two items', function(){
-      expect( spyTester.bubbleSort([8, 5]) ).toEqual( [5, 8] );
-      expect(spyTester.swap.calls.count()).toEqual(1);
+      expect(bubbleSort([8, 5]) ).toEqual( [5, 8] );
+      expect(swap.calls.count()).toEqual(1);
     });
     it('handles items that are already in order', function(){
-      expect( spyTester.bubbleSort([1, 5, 8])).toEqual( [1, 5, 8] );
-      expect(spyTester.swap.calls.count()).toEqual(3);
+      expect(bubbleSort([1, 5, 8])).toEqual( [1, 5, 8] );
+      expect(swap.calls.count()).toEqual(3);
     });
     it('handles items in a large array', function(){
-      expect( spyTester.bubbleSort([3, 10, 8, 9, 2, 15, 1])).toEqual( [1, 2, 3, 8, 9, 10, 15] );
-      expect(spyTester.swap.calls.count()).toEqual(21);
+      expect(bubbleSort([3, 10, 8, 9, 2, 15, 1])).toEqual( [1, 2, 3, 8, 9, 10, 15] );
+      expect(swap.calls.count()).toEqual(21);
     });
     it('handles an array with duplicates', function(){
-      expect( spyTester.bubbleSort([5, 1, 2, 5, 1])).toEqual( [1, 1, 2, 5, 5] );
-      expect(spyTester.swap.calls.count()).toEqual(10);
+      expect(bubbleSort([5, 1, 2, 5, 1])).toEqual( [1, 1, 2, 5, 5] );
+      expect(swap.calls.count()).toEqual(10);
     });
   });
