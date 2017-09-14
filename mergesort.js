a315daec88ff68c43d176ad284f24f87ada59500
Mary Irwin <maldenirwin@gmail.com>
2017-09-14 15:22:02 -0400

    new changes

--- mergesort.js
+++ mergesort.js
@@ -4,4 +4,25 @@ function split(wholeArray) {
     var firstHalf = wholeArray.slice(0,wholeArray.length/2);
     var secondHalf = wholeArray.slice(wholeArray.length/2);
     return [firstHalf, secondHalf];
+
+  }
+
+  function merge(first,second) {
+    var firstIndex = 0;
+    var secondIndex = 0;
+    var result = [];
+
+    while(firstIndex < first.length && secondIndex < second.length) {
+      if(first[firstIndex] <= second[secondIndex]){
+        result.push(first[firstIndex])
+        firstIndex ++
+      } else {
+        result.push(second[secondIndex])
+        secondIndex ++
+      }
+    }
+    var slice1 = first.slice(firstIndex)
+    var slice2 = second.slice(secondIndex)
+    result = result.concat(slice1).concat(slice2)
+    return result;
   }
