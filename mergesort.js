927f340ad4339898544d20e81861949a5bc0cafd
Caryn McCarthy <cmccarthy15@gmail.com>
2017-09-14 16:08:59 -0400

    updated with some es6 syntax

--- mergesort.js
+++ mergesort.js
@@ -1,37 +1,34 @@
 function split(wholeArray) {
-
-      /* your code here to define the firstHalf and secondHalf arrays */
       const center = wholeArray.length / 2,
          firstHalf = wholeArray.slice(0, center), secondHalf = wholeArray.slice(center);
       return [firstHalf, secondHalf];
-
     }
 
-    function merge(first, second) {
-      let firstIndex = 0,
-        secondIndex = 0,
-        result = [];
+function merge(first, second) {
+  let firstIndex = 0,
+    secondIndex = 0,
+    result = [];
 
-      while (firstIndex < first.length && secondIndex < second.length) {
-        if (first[firstIndex] <= second[secondIndex]){
-          result.push(first[firstIndex]);
-          firstIndex++;
-        } else {
-          result.push(second[secondIndex]);
-          secondIndex++;
-        }
-      }
-      const firstRemain = first.slice(firstIndex), secondRemain = second.slice(secondIndex);
-      result = result.concat(firstRemain).concat(secondRemain);
-      return result;
+  while (firstIndex < first.length && secondIndex < second.length) {
+    if (first[firstIndex] <= second[secondIndex]){
+      result.push(first[firstIndex]);
+      firstIndex++;
+    } else {
+      result.push(second[secondIndex]);
+      secondIndex++;
     }
+  }
+  const firstRemain = first.slice(firstIndex), secondRemain = second.slice(secondIndex);
+  result = result.concat(firstRemain).concat(secondRemain);
+  return result;
+}
 
-    function mergeSort(array){
-      if (array.length < 2){
-        return array;
-      }
-      const splits = split(array),
-        left = splits[0],
-        right = splits[1];
-      return merge(mergeSort(left), mergeSort(right));
-    }
+function mergeSort(array){
+  if (array.length < 2){
+    return array;
+  }
+  const splits = split(array),
+    left = splits[0],
+    right = splits[1];
+  return merge(mergeSort(left), mergeSort(right));
+}
